6d54b9055ecd98e7511487e760e1240664d84cb1
Martin Mimeault <54674451+mimeault@users.noreply.github.com>
2023-02-06 12:58:50 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CDTK Documentation'
+copyright = '2023, Natural resouces Canada'
+author = 'Martin Mimeault'
 
 release = '0.1'
 version = '0.1.0'
